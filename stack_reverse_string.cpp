431181b65f62d6d4dde5f203aa0811e83e58f788
Harshita Kanal <harshita.kgv@gmail.com>
2020-09-26 21:56:52 +0530

    Create stack_reverse_string.cpp

---
+++ stack_reverse_string.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+void reverse(char *str, int len);
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        char str[10000];
+        cin>>str;
+        long long int len=strlen(str);
+        reverse(str,len);
+        cout<<str;
+        cout<<endl;
+    }
+        return 0;
+}
+// Driver Code Ends
+
+
+void reverse(char *str, int len)
+{
+    int i;
+    stack<char> s;
+    for(i = 0; i < len; i++){
+        s.push(str[i]);
+    }
+    i = 0;
+    while(!s.empty()){
+        str[i] = s.top();
+        s.pop();
+        i++;
+    }
+
+}
