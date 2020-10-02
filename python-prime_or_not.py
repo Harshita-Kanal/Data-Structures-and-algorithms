2948833fc91db3c4b4c7202d46d837cc57d1aa62
Arsalan Sefidgar <dr.sefidgar@gmail.com>
2020-10-02 15:22:23 +0330

    Refactor old prime check algorithm

--- python-prime_or_not.py
+++ python-prime_or_not.py
@@ -1,20 +1,16 @@
-# Enter a number
-  
+import math
+
 num = int(input("Enter a number = "))
-  
-# If given number is greater than 1 
-if num > 1: 
-      
-   # Iterate from 2 to n / 2  
-   for i in range(2, num): 
-         
-       # If num is divisible by any number between  
-       # 2 and n / 2, it is not prime  
-       if (num % i) == 0: 
-           print(num, "is not a prime number") 
-           break
-   else: 
-       print(num, "is a prime number") 
-  
-else: 
-   print(num, "is not a prime number")
+
+def is_prime(number: int) -> bool:
+    if number < 2:
+        return False
+    if number < 4:
+        return True
+    if number % 2 == 0:
+        return False
+    
+    odd_numbers = range(3, int(math.sqrt(number)) + 1, 2)
+    return not any(number % i == 0 for i in odd_numbers)
+
+print(f"{num} is a prime number" if is_prime(num) else f"{num} is not a prime number")
