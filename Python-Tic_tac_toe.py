8d6f46f6aaffc987b39e2aa68c41cb33c9887b22
Caro Oviedo <60893793+caro-oviedo@users.noreply.github.com>
2020-10-03 17:02:16 -0300

    Python_Tic_tac_toe

--- Python-Tic_tac_toe.py
+++ Python-Tic_tac_toe.py
@@ -1,6 +1,6 @@
 
 def print_board(board): 
-    print(f"{board['top-l']}|{board['top-m']}|{board['top-r']} ")
+    print(f"{board['top-l']}|{board['top-m']}|{board['top-r']} ")   #print a nice board
     print("-+--+-")
     print(f"{board['mid-l']}|{board['mid-m']}|{board['mid-r']} ")
     print("-+--+-")
@@ -26,23 +26,23 @@ the_board = {'top-l': '?', 'top-m': '?', 'top-r': '?',
 print("******TIC TAC TOE *******\n")
 
 turn = 'X'
-for i in range(10):
+for i in range(10):      #repeat 9 times
     print_board(the_board)
     print(f"\nNow is {turn}'s turn. What's your move? (Example: top-l , mid-m, low-r ")
     print("Press 'q' to quit at any time.")
-    move = input().lower().rstrip()
+    move = input().lower().rstrip()     #turn lowercase and erase spaces 
 
     if move == 'q': 
         break
-    if move not in the_board.keys():
+    if move not in the_board.keys():     #validate user's input. 
       print("***That's not a valid move***. Try again.\n")
       continue
     the_board[move] = turn
-    win = check_winner(the_board, turn)
+    win = check_winner(the_board, turn)  #check if there's a winner
     if win == True: 
       break
   
-    if turn == 'X':
+    if turn == 'X':      #change turn
         turn = 'O'
     else: 
         turn ='X'
