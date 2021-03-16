4c6465ebfdc7c7824a372bc5b1e8896921241695
Sydnie Nguyen <sydnienguyen@csu.fullerton.edu>
2021-03-15 21:29:30 -0700

    testing

---
+++ LinearSearch/LinearSearch/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  LinearSearch
+//
+//  Created by Sydnie Nguyen on 3/15/21.
+// input: an array x of integer and query needed to be found q
+// output: index of the query or None if no query found
+
+#include <stdio.h>
+
+
+int main(int argc, const char * argv[]) {
+    //for now
+  
+    
+    
+    
+    
+    
+    // printf("Enter an array of integers: ");
+    
+    return 0;
+}
